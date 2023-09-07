b628983e8a397a2478a8ed31ccb1febb6bb68d38
Julius Gerba <juliusgerba@gmail.com>
2023-09-07 12:30:40 +0300

    address panel added

---
+++ src/components/AddressPanel.js
@@ -0,0 +1,16 @@
+function AddressPanel() {
+    return (
+        <article>
+            <div>
+                <span>zakaz@loverflower.by</span>
+                <p>Доставка 24/7 по договоренности с оператором</p>
+            </div>
+            <div>
+                <span>ул. Тимирязева 67</span>
+                <p>10:00 до 21:00 {<br />}без выходных</p>
+            </div>
+        </article>
+    );
+}
+
+export default AddressPanel;
