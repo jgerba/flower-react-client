9e1e6eb3d9f4e4ca29512bf5b913d8743276c852
Julius Gerba <juliusgerba@gmail.com>
2023-10-03 21:26:48 +0300

    upd

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,9 +2,15 @@ import { NavLink } from 'react-router-dom';
 
 import classes from './Logo.module.css';
 
-function Logo() {
+function Logo(props) {
     return (
-        <NavLink className={`${classes.logo} `} to="/" end>
+        <NavLink
+            className={`${classes.logo} ${
+                props.className ? props.className : ''
+            }`}
+            to="/"
+            end
+        >
             <p className={classes['logo-L']}>l</p>
             <p className={classes['logo-F']}>f</p>
         </NavLink>
