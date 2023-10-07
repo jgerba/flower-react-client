6244d3dc26eb436fb7d1caa5d6b9e21babd4953f
Julius Gerba <juliusgerba@gmail.com>
2023-10-07 16:01:07 +0300

    style upd

--- src/components/FilterItem.jsx
+++ src/components/FilterItem.jsx
@@ -3,7 +3,9 @@ import classes from './FilterItem.module.css';
 function FilterItem(props) {
     return (
         <li className={classes.item}>
-            <label htmlFor={props.name}>{props.children}</label>
+            <label className={classes.item} htmlFor={props.name}>
+                {props.children}
+            </label>
             <input id={props.name} name={props.name} type="checkbox" />
         </li>
     );
