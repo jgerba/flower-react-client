a7bf63cbeeff8330048e782640b560bd3db8a060
Julius Gerba <juliusgerba@gmail.com>
2023-10-07 17:16:57 +0300

    class upd

--- src/components/BouquetCard.jsx
+++ src/components/BouquetCard.jsx
@@ -3,19 +3,20 @@ import MenuBtn from './UI/MenuBtn';
 import classes from './BouquetCard.module.css';
 
 function BouquetCard(props) {
+    console.log(props.src);
     return (
         <div
             className={`${classes.card} ${
-                props.className ? classes[props.className] : ''
+                props.className ? props.className : ''
             }`}
         >
             <img
                 className={classes.image}
-                src={`${props.src}`}
+                src={props.src}
                 alt={`Букет ${props.title}`}
             ></img>
             <p className={classes.title}>{props.title}</p>
-            <p className={classes.price}>{props.price}</p>
+            <p className={classes.price}>{`${props.price} ₽`}</p>
             <MenuBtn className={classes.button} blank={true}>
                 В корзину
             </MenuBtn>
