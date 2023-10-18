e85321de0e1e45af3bca4b76ed15d0f0f9de5ccb
Julius Gerba <juliusgerba@gmail.com>
2023-10-18 22:43:27 +0300

    checkout page added

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,14 @@
+import SectionHeader from '../components/UI/SectionHeader';
+
+import classes from './Checkout.module.css';
+
+function Checkout(props) {
+    <main>
+        <div className={classes.header}>
+            <SectionHeader>оформление</SectionHeader>
+            <SectionHeader smallHeader={true}>заказа</SectionHeader>
+        </div>
+    </main>;
+}
+
+export default Checkout;
