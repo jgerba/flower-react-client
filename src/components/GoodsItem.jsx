21d6255f789d53e38ca42ce86193a0750148f830
Julius Gerba <juliusgerba@gmail.com>
2023-10-28 14:51:34 +0300

    remove item btn added

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -1,4 +1,5 @@
 import classes from './GoodsItem.module.css';
+import bin from '../svg/bin.svg';
 
 function GoodsItem(props) {
     return (
@@ -6,11 +7,18 @@ function GoodsItem(props) {
             className={`${classes.item} ${
                 props.className ? props.className : ''
             }`}
-            onClick={() => props.onClick()}
+            onClick={props.onClick}
         >
             <p className={classes.title}>{props.title}</p>
             <p className={classes.price}>{props.price}</p>
             <p className={classes.descr}>{props.descr}</p>
+
+            <button
+                className={classes['remove-btn']}
+                onClick={() => props.onRemove()}
+            >
+                <img src={bin} alt="Мусорная корзина" />
+            </button>
         </div>
     );
 }
