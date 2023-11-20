ab745d19d78487b5e319419e9ad763d67fdce342
Julius Gerba <juliusgerba@gmail.com>
2023-11-20 22:51:33 +0300

    404 page added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,33 @@
-import { useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 
-function ErrorPage(params) {
-    // reset scroll position
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+import classes from './Error.module.css';
+import logo from '../svg/lover flower-blue.svg';
 
-    return <></>;
+function ErrorPage() {
+    return (
+        <main className={classes.main}>
+            <h2 className={classes.header}>4</h2>
+            <h2 className={classes.header}>0</h2>
+            <h2 className={classes.header}>4</h2>
+
+            <div className={classes.text}>
+                <p>Ошибка 404</p>
+                <p>Упс...Такой страницы нет</p>
+                <NavLink className={classes.link} to="/">
+                    на главную
+                </NavLink>
+            </div>
+
+            <img className={classes.logo} src={logo} alt="Lower Flower logo" />
+
+            <div className={classes['decor-flower-left']}></div>
+            <div className={classes['decor-flower-right']}></div>
+
+            <div className={classes['decor-ellipse-top']}></div>
+            <div className={classes['decor-ellipse-bottom-left']}></div>
+            <div className={classes['decor-ellipse-bottom-right']}></div>
+        </main>
+    );
 }
 
 export default ErrorPage;
