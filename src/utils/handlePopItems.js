f44c1c1adbb86314168ce348da60ded91094b439
Julius Gerba <juliusgerba@gmail.com>
2023-12-03 22:04:21 +0300

    bug fixed

--- src/utils/handlePopItems.js
+++ src/utils/handlePopItems.js
@@ -1,13 +1,20 @@
 function handlePopItems(data, quantity = 3) {
+    if (!data) return;
+
     const popularItems = [];
 
     // choose 3 random items as 'popular'
     for (let i = 0; i < quantity; i++) {
-        let index = Math.round(Math.random() * data.length - 1);
+        let index = Math.round(Math.random() * (data.length - 1));
+
+        console.log(index);
 
         // change index if the same item
-        while (popularItems.some(item => item._id === data[index]._id)) {
-            index = Math.round(Math.random() * data.length - 1);
+        while (
+            popularItems.length > 0 &&
+            popularItems.some(item => item._id === data[index]._id)
+        ) {
+            index = Math.round(Math.random() * (data.length - 1));
         }
 
         popularItems.push(data[index]);
